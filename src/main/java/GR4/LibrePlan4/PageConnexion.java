84c4e4ce37e2e9ea4a4fb57ac9da74c9a6317e94
MarineCoutand0 <mcoutand@henix.fr>
2019-07-18 15:36:31 +0200

    classe test

--- src/main/java/GR4/LibrePlan4/PageConnexion.java
+++ src/main/java/GR4/LibrePlan4/PageConnexion.java
@@ -1,9 +1,22 @@
 package GR4.LibrePlan4;
 
-public class PageConnexion {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class PageConnexion extends PageBandeau {
+
+	@FindBy (xpath="//input[@name='j_username']")
+	private WebElement username;
+	
+	@FindBy (xpath="//input[@name='j_password']")
+	private WebElement password;
+	
+	@FindBy (xpath="//input[@type='submit']")
+	private WebElement bouton_submit;
+	
 	public PageConnexion() {
-				
+		super();
+		
 	}
 	
 }
---
+++ src/test/java/GR4/LibrePlan4/Test.java
@@ -0,0 +1,5 @@
+package GR4.LibrePlan4;
+
+public class Test {
+
+}
