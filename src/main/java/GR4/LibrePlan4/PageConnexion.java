46a33d7f232a5431a37f65f55b65d1e5c10815d6
MarineCoutand0 <mcoutand@henix.fr>
2019-07-18 16:14:39 +0200

    connexion

--- src/main/java/GR4/LibrePlan4/PageConnexion.java
+++ src/main/java/GR4/LibrePlan4/PageConnexion.java
@@ -1,15 +1,19 @@
 package GR4.LibrePlan4;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PageConnexion extends PageBandeau {
 
+	
+	
 	@FindBy (xpath="//input[@name='j_username']")
-	private WebElement username;
+	private WebElement chmps_username;
 	
 	@FindBy (xpath="//input[@name='j_password']")
-	private WebElement password;
+	private WebElement chmps_password;
 	
 	@FindBy (xpath="//input[@type='submit']")
 	private WebElement bouton_submit;
@@ -18,5 +22,13 @@ public class PageConnexion extends PageBandeau {
 		super();
 		
 	}
+	
+	public PageAccueil seConnecter(WebDriver driver, String username, String password) {
+		Outils.renseignerChamp(chmps_username, username);
+		Outils.renseignerChamp(chmps_password, password);
+		bouton_submit.submit();
+		return PageFactory.initElements(driver, PageAccueil.class);
+	}
+	
 	
 }
---
+++ src/test/java/GR4/LibrePlan4/Test_connexion.java
@@ -0,0 +1,36 @@
+package GR4.LibrePlan4;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class Test_connexion {
+	
+	WebDriver driver;
+	String b ="chrome";
+	String url ="http://localhost:8090/libreplan";
+	
+	@Before
+	public void setUp() {
+		driver = Outils.choisirNavigateur(b);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+//	@After
+//	public void tearDown() {
+//		driver.close();
+//	}
+	
+	@Test
+	public void Connexion() {
+		PageConnexion pageConnexion = PageFactory.initElements(driver, PageConnexion.class);
+		driver.get(url);
+		pageConnexion.seConnecter(driver, "admin", "admin");
+	}
+
+}
